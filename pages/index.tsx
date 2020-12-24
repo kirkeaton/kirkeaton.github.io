e7adec32e158b6518c613a9a227183ba70e04eff
Kirk Eaton <kirk.eaton@gmail.com>
2020-12-24 14:25:46 -0500

    fix: Add NEXT_PUBLIC_ prefix to all environment variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,127 +110,123 @@ const GridBox = (props: GridBoxProps) => (
   </Box>
 );
 
-const Home = () => {
-  const {
-    META_DESCRIPTION,
-    META_ROBOTS,
-    META_TITLE,
-    META_URL,
-    META_VIEWPORT,
-  } = process.env;
+const Home = () => (
+  <>
+    <Head>
+      <title>{process.env.NEXT_PUBLIC_META_TITLE}</title>
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content={process.env.NEXT_PUBLIC_META_DESCRIPTION}
+      />
+      <meta name="robots" content={process.env.NEXT_PUBLIC_META_ROBOTS} />
+      <meta name="viewport" content={process.env.NEXT_PUBLIC_META_VIEWPORT} />
+      <link rel="canonical" href={process.env.NEXT_PUBLIC_META_URL} />
 
-  return (
-    <>
-      <Head>
-        <title>{META_TITLE}</title>
-        <meta charSet="utf-8" />
-        <meta name="description" content={META_DESCRIPTION} />
-        <meta name="robots" content={META_ROBOTS} />
-        <meta name="viewport" content={META_VIEWPORT} />
-        <link rel="canonical" href={META_URL} />
+      {/* Open Graph */}
+      <meta
+        name="og:description"
+        content={process.env.NEXT_PUBLIC_META_DESCRIPTION}
+      />
+      <meta name="og:title" content={process.env.NEXT_PUBLIC_META_TITLE} />
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content={process.env.NEXT_PUBLIC_META_URL} />
 
-        {/* Open Graph */}
-        <meta name="og:description" content={process.env.META_DESCRIPTION} />
-        <meta name="og:title" content={META_TITLE} />
-        <meta name="og:type" content="website" />
-        <meta name="og:url" content={META_URL} />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={process.env.NEXT_PUBLIC_META_TITLE} />
+      <meta
+        name="twitter:description"
+        content={process.env.NEXT_PUBLIC_META_DESCRIPTION}
+      />
+    </Head>
 
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={META_TITLE} />
-        <meta
-          name="twitter:description"
-          content={process.env.META_DESCRIPTION}
-        />
-      </Head>
+    <Box minW="md" maxW="6xl" mx="auto" pt={4} px={8}>
+      <Stack alignItems="center" isInline justifyContent="space-between">
+        <Heading>Kirk Eaton</Heading>
 
-      <Box minW="md" maxW="6xl" mx="auto" pt={4} px={8}>
-        <Stack alignItems="center" isInline justifyContent="space-between">
-          <Heading>Kirk Eaton</Heading>
-
-          <Box display={['block', 'block', 'none', 'none']}>
-            <MobileMenu />
-          </Box>
-
-          <Box display={['none', 'none', 'block', 'block']}>
-            <DesktopMenu />
-          </Box>
-        </Stack>
-      </Box>
-
-      <Box minWidth="md" maxW="6xl" mx="auto" px={8}>
-        <Box className="about">
-          <Heading py={4} size="lg">
-            About Me
-          </Heading>
-
-          <Text fontWeight="light">
-            Experienced{' '}
-            <Text as="span" fontWeight="bold">
-              Software Engineer
-            </Text>{' '}
-            with a demonstrated history of shipping efficient &amp; reliable
-            software.
-          </Text>
+        <Box display={['block', 'block', 'none', 'none']}>
+          <MobileMenu />
         </Box>
 
-        <Box className="projects">
-          <Heading py={4} size="lg">
-            Projects &amp; Contributions
-          </Heading>
-
-          <SimpleGrid
-            justifyContent="center"
-            minChildWidth="20rem"
-            spacing="1rem"
-          >
-            <GridBox
-              title="Particle Setup"
-              description="Library that sends commands to Particle devices."
-              url="https://github.com/kirkeaton/particle_setup"
-              isAuthor
-            />
-
-            <GridBox
-              title="Read Pubspec"
-              description="Library that reads a pubspec.yaml file."
-              url="https://github.com/kirkeaton/read-pubspec"
-              isAuthor
-            />
-
-            <GridBox
-              title="Sudoku Image Solver"
-              description="Program that processes and solves Sudoku puzzles."
-              url="https://github.com/kirkeaton/sudoku-image-solver"
-              isAuthor
-            />
-
-            <GridBox
-              title="Mongoose History"
-              description="Plugin that keeps a history of all changes of a document."
-              url="https://github.com/nassor/mongoose-history"
-            />
-
-            <GridBox
-              title="Flutter Circular Slider"
-              description="Customizable circular slider for Flutter."
-              url="https://github.com/davidanaya/flutter-circular-slider"
-            />
-
-            <GridBox
-              title="FOAM Framework"
-              description="Modeling tool and class based object system."
-              url="https://github.com/foam-framework/foam2"
-            />
-          </SimpleGrid>
+        <Box display={['none', 'none', 'block', 'block']}>
+          <DesktopMenu />
         </Box>
+      </Stack>
+    </Box>
+
+    <Box minWidth="md" maxW="6xl" mx="auto" px={8}>
+      <Box className="about">
+        <Heading py={4} size="lg">
+          About Me
+        </Heading>
+
+        <Text fontWeight="light">
+          Experienced{' '}
+          <Text as="span" fontWeight="bold">
+            Software Engineer
+          </Text>{' '}
+          with a demonstrated history of shipping efficient &amp; reliable
+          software.
+        </Text>
       </Box>
 
-      <Center minWidth="md" maxW="6xl" mx="auto" py={4} px={8}>
-        Copyright &#169; 2020 Kirk Eaton
-      </Center>
-    </>
-  );
-};
+      <Box className="projects">
+        <Heading py={4} size="lg">
+          Projects &amp; Contributions
+        </Heading>
+
+        <SimpleGrid
+          justifyContent="center"
+          minChildWidth="20rem"
+          spacing="1rem"
+        >
+          <GridBox
+            title="Particle Setup"
+            description="Library that sends commands to Particle devices."
+            url="https://github.com/kirkeaton/particle_setup"
+            isAuthor
+          />
+
+          <GridBox
+            title="Read Pubspec"
+            description="Library that reads a pubspec.yaml file."
+            url="https://github.com/kirkeaton/read-pubspec"
+            isAuthor
+          />
+
+          <GridBox
+            title="Sudoku Image Solver"
+            description="Program that processes and solves Sudoku puzzles."
+            url="https://github.com/kirkeaton/sudoku-image-solver"
+            isAuthor
+          />
+
+          <GridBox
+            title="Mongoose History"
+            description="Plugin that keeps a history of all changes of a document."
+            url="https://github.com/nassor/mongoose-history"
+          />
+
+          <GridBox
+            title="Flutter Circular Slider"
+            description="Customizable circular slider for Flutter."
+            url="https://github.com/davidanaya/flutter-circular-slider"
+          />
+
+          <GridBox
+            title="FOAM Framework"
+            description="Modeling tool and class based object system."
+            url="https://github.com/foam-framework/foam2"
+          />
+        </SimpleGrid>
+      </Box>
+    </Box>
+
+    <Center minWidth="md" maxW="6xl" mx="auto" py={4} px={8}>
+      Copyright &#169; 2020 Kirk Eaton
+    </Center>
+  </>
+);
 
 export default Home;
