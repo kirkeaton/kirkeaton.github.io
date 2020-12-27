7c7e6ec8de7ce4a81ca68e370a8a18d266379838
Kirk Eaton <kirk.eaton@gmail.com>
2020-12-27 12:41:24 -0500

    feat: Add custom document to add gtag.js to all pages

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import _Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class Document extends _Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* gtag.js for Google Analytics */}
+          {process.env.VERCEL_ENV === 'production' && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+              `,
+                }}
+              ></script>
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
