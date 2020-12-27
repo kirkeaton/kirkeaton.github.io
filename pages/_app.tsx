a4705e5997c26cebcf9007890a6fd292ef3890f7
Kirk Eaton <contact@kirkeaton.ca>
2020-12-27 12:48:08 -0500

    feat: Add tracking of pageviews

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const App = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
-);
+import * as gtag from '../lib/gtag';
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
 
 export default App;
